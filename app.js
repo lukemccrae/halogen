4887ee4649ce156c14f72a68ecbdcb08e36b2bfe
“lukemccrae” <lukemccrae@gmail.com>
2021-04-03 10:06:19 -0700

    smooth scrolling, add sliders

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log("I picked a hell of a day to quit amphetamines")
+
+function showDemo() {
+    //smooth scroll down to the demo area
+    var scrollDiv = document.getElementById("demo").offsetTop;
+    window.scrollTo({ top: scrollDiv, behavior: 'smooth'});
+}
+
+function ySlider(input) {
+    console.log(input)
+}
+
+function xSlider(input) {
+    console.log(input)
+}
